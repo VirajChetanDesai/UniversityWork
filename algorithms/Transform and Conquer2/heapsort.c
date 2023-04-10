85bfca1ca04ea4b6f2859874acb9990a26664ccc
Viraj Desai <82761923+VirajChetanDesai@users.noreply.github.com>
2023-04-11 01:45:30 +0530

    heap

--- algorithms/Transform and Conquer2/heapsort.c
+++ algorithms/Transform and Conquer2/heapsort.c
@@ -22,6 +22,16 @@ void build_heap(int* heap,int* array, int size){
     }
 }
 
+void heapify(int *heap,int size){
+    for(int i = size; i>1 ; i--){
+        int j = i;
+        while (j > 1 && heap[j/2] < heap[j]) {
+            swap(&heap[j], &heap[j/2]);
+            j = j/2;
+        }
+    }
+}
+
 int main(){
     int size;
     printf("Enter the number of elements: ");
@@ -35,7 +45,19 @@ int main(){
     build_heap(heap, array, size);
     printf("Heap:\n");
     for(int i = 1 ; i <= size ; i++){
-        printf("%d", heap[i]);
+        printf("%d\t", heap[i]);
+    }
+    int* finarray = (int*) calloc(size,sizeof(int));
+    int k = 0;
+    while(heap[1]!=0){
+        finarray[k++] = heap[1];
+        heap[1]=heap[size-k];
+        heap[size-k] = 0;
+        heapify(heap,size);
+    }
+    printf("Final Array :");
+    for(int i = 0 ; i <size ; i++){
+        printf("%d\t",finarray[i]);
     }
     return 0;
 }
