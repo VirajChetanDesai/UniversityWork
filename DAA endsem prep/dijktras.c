cc54351c81e074b33dab2fe916d758a8491bbe75
Viraj Desai <82761923+VirajChetanDesai@users.noreply.github.com>
2023-05-07 19:43:38 +0530

    dijktras

--- DAA endsem prep/dijktras.c
+++ DAA endsem prep/dijktras.c
@@ -1,62 +1,83 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include<stdio.h>
 
-#define MAX_VERTICES 100
+#include<stdlib.h>
 
-int min_distance(int dist[], int visited[], int n) {
-    int min_index, min_distance = INT_MAX;
-    for (int i = 0; i < n; i++) {
-        if (!visited[i] && dist[i] < min_distance) {
-            min_distance = dist[i];
-            min_index = i;
-        }
-    }
+#include<limits.h>
 
-    return min_index;
-}
-
-void dijkstra(int graph[MAX_VERTICES][MAX_VERTICES], int n, int start) {
-    int dist[n], visited[n];
-    for (int i = 0; i < n; i++) {
-        dist[i] = INT_MAX;
-        visited[i] = 0;
+void dijktras(int n, int ** cost) {
+  int * parent = (int * ) calloc(n, sizeof(int)); // keeps track of parent
+  int * processed = (int * ) calloc(n, sizeof(int)); //keeps track of already processed
+  long long * value = (long long * ) calloc(n, sizeof(long long)); //keeps track of min cost to reach particular node
+  for (int i = 0; i < n; i++) {
+    parent[i] = -1;
+    processed[i] = 0; //0 = false 1 = true
+    value[i] = LONG_MAX; //effectively initial cost to reach anything is infinite
+  }
+  processed[0] = 1;
+  value[0] = 0; // since we will always start from first node in the list (can be changed)
+  int current = 0; //index of current vertice
+  int nodesreached = 1;
+  while (nodesreached < n) {
+    for (int j = 0; j < n; j++) {
+      if (processed[j] != 1 && cost[current][j] != 0 && (value[current] + cost[current][j] < value[j])) {
+        printf("%d %d\n", value[current], cost[current][j]);
+        value[j] = value[current] + cost[current][j];
+        parent[j] = current;
+      }
+      printf("%d for %d\n", value[1], parent[1]);
     }
-    dist[start] = 0;
-    for (int i = 0; i < n - 1; i++) {
-        int u = min_distance(dist, visited, n);
-        visited[u] = 1;
-        for (int v = 0; v < n; v++) {
-            if (graph[u][v] && !visited[v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v]) {
-                dist[v] = dist[u] + graph[u][v];
-            }
-        }
+    long min = LONG_MAX;
+    for (int j = 0; j < n; j++) {
+      if (processed[j] != 1 && value[j] < min) {
+        current = j;
+        min = value[j];
+      }
     }
-    printf("Vertex\tDistance from Start\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t%d\n", i, dist[i]);
-    }
+    processed[current] = 1;
+    nodesreached++;
+  }
+  for (int i = 1; i < n; i++) {
+    printf("%d - %d\n", parent[i], i);
+  }
+  for (int i = 0; i < n; i++) {
+    printf("cost for reaching vertex %d : %d\n", i, value[i]);
+  }
+  free(parent);
+  free(processed);
+  free(value);
 }
 
 int main() {
-    int n, start;
-
-    printf("Enter the number of vertices: ");
-    scanf("%d", &n);
-
-    int graph[MAX_VERTICES][MAX_VERTICES];
-
-    printf("Enter the adjacency matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &graph[i][j]);
-        }
+  int n;
+  printf("Enter size of the graph : ");
+  scanf("%d", & n);
+  int ** cost = (int ** ) calloc(n, sizeof(int * ));
+  if (cost == NULL) {
+    printf("Memory allocation failed\n");
+    exit(1);
+  }
+  for (int i = 0; i < n; i++) {
+    int * rowcost = (int * ) calloc(n, sizeof(int));
+    if (rowcost == NULL) {
+      printf("Memory allocation failed\n");
+      exit(1);
     }
+    for (int j = 0; j < n; j++) {
+      scanf("%d", & rowcost[j]);
+      if (rowcost[j] == 0) { // Handle unreachable vertices
+        rowcost[j] = INT_MAX;
+      }
+    }
+    cost[i] = rowcost;
+  }
 
-    printf("Enter the start vertex: ");
-    scanf("%d", &start);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      printf("%d\t", cost[i][j]);
+    }
+    printf("\n");
+  }
 
-    dijkstra(graph, n, start);
-
-    return 0;
+  dijktras(n, cost);
+  return 0;
 }
